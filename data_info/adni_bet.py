a5c8b116e863db7438a3966f39f95bf6a80ae13c
polina <buzoilic@gmail.com>
2021-05-19 15:57:04 +0300

    add adni bet

---
+++ data_info/adni_bet.py
@@ -0,0 +1,27 @@
+import nipype.interfaces.fsl as fsl
+from nilearn.plotting import plot_anat
+import nilearn.plotting
+import os
+
+PATH_TO_MRI = '/data/caps/subjects'
+# sub-ADNI099S0291_ses-M00_T1w_space-MNI152NLin2009cSym_desc-Crop_res-1x1x1_T1w.nii.gz
+# sub-ADNI128S0528_ses-M00_T1w_space-MNI152NLin2009cSym_desc-Crop_res-1x1x1_T1w.nii.gz
+def adni_fsl_bet():
+    i = 0
+    for sub in os.listdir(PATH_TO_MRI):
+        for ses in sub:
+                full_path = '{}/{}/{}/t1_linear/sub-{}_ses-{}_T1w_space-MNI152NLin2009cSym_desc-Crop_res-1x1x1_T1w.nii.gz'.format(PATH_TO_MRI, sub, ses, sub,ses)
+                if os.path.exists(full_path):
+                    out_path = '/home/ADNI/{}/{}/{}/t1_linear/sub-{}_ses-{}_T1w_space-MNI152NLin2009cSym_desc-Crop_res-1x1x1_T1w_brain.nii.gz'.format(PATH_TO_MRI,sub, ses, sub,ses)
+                    skullstrip = fsl.BET(in_file=full_path, out_file=out_path,
+                        mask =True)
+                    res = skullstrip.run()
+                    i=+1
+                    print(i)
+                else:
+                    print("Can't find file in {}".format(full_path))
+
+
+if __name__ == '__main__':
+
+    adni_fsl_bet()
